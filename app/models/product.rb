942bbd90564276f849e4b7c64eae90e699c9c900
Hano <hinsul3113@gmail.com>
2020-04-24 10:30:59 -0700

    Product model validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+    validates :title, {
+        presence: true, 
+        uniqueness: { case_sensitive: false}
+    }
+
+    validates :price, {
+        numericality: { greater_than_or_equal_to: 0}
+    }
+    
+    validates :description, {
+        presense: true, 
+        length: { minimum: 10}
+    }
 end
